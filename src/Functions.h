fecae78eecb1bf7e80b95da0e142e8efe306ee0a
Jason <jasonmjohnson1@gmail.com>
2017-07-24 10:50:01 -0400

    Create Functions.h

---
+++ src/Functions.h
@@ -0,0 +1,8 @@
+
+KAPC_STATE AttachProcess(HANDLE ProcessId);
+
+NTSTATUS WriteMemory(PVOID Destination, PVOID Buffer, SIZE_T BufferSize, ULONG fProtect, KPROCESSOR_MODE ProcessorMode);
+
+NTSTATUS ReadMemory(PVOID Buffer, PVOID MemoryToRead, SIZE_T Size);
+
+void DetachProcess(KAPC_STATE* apc);
