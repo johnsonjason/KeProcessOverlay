ac208c76fa97f9e6b731ed5da513fba288d118db
Jason <jasonmjohnson1@gmail.com>
2017-07-28 02:34:09 -0400

    Create SignatureScan.h

---
+++ src/SignatureScan.h
@@ -0,0 +1,12 @@
+#ifndef SIGNATURESCAN
+#define SIGNATURESCAN
+
+typedef struct _Signature
+{
+	DWORD64 val1;
+	DWORD64 val2;
+} Signature;
+
+PVOID GetSignatureBase(PEPROCESS pe_process, DWORD64 BaseStart, DWORD64 BaseEnd, Signature ScanSignature, SIZE_T RegionSize, DWORD32 Type);
+
+#endif
