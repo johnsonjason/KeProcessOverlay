3e6bb94ac99332556ec2a7bdb5a64f6af897d36d
Jason <jasonmjohnson1@gmail.com>
2017-07-28 02:48:13 -0400

    Update SignatureScan.h

--- src/SignatureScan.h
+++ src/SignatureScan.h
@@ -7,6 +7,8 @@ typedef struct _Signature
 	DWORD64 val2;
 } Signature;
 
-PVOID GetSignatureBase(PEPROCESS pe_process, DWORD64 BaseStart, DWORD64 BaseEnd, Signature ScanSignature, SIZE_T RegionSize, DWORD32 Type);
+PVOID GetSignatureBase(HANDLE ProcessId, DWORD64 BaseStart, 
+		       DWORD64 BaseEnd, Signature ScanSignature, 
+		       SIZE_T RegionSize, DWORD32 Type);
 
 #endif
