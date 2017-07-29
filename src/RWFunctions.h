1fcd0962e7994d4c87b661c3616bc39c3cff61cd
Jason <jasonmjohnson1@gmail.com>
2017-07-28 21:36:39 -0400

    Update RWFunctions.h

--- src/RWFunctions.h
+++ src/RWFunctions.h
@@ -1,6 +1,10 @@
 #ifndef FUNCTIONS
 #define FUNCTIONS
 
+#include <ntifs.h>
+#include <ntdef.h>
+#include <wdf.h>
+
 KAPC_STATE AttachProcess(HANDLE ProcessId);
 
 NTSTATUS WriteMemory(PVOID Destination, PVOID Buffer, SIZE_T BufferSize, ULONG fProtect, KPROCESSOR_MODE ProcessorMode);
